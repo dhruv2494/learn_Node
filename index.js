5c8a15c18e2ea4b68015046bab0318994d3dd4e0
dhruv2494 <bhuvadhruv24@gmail.com>
2024-02-27 15:29:37 +0530

    security_15

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const connectDB = require("./src/connectMongo");
 
 require("dotenv").config();
 
@@ -16,44 +17,19 @@ app.use(cors());
 //   .connect(process.env.MONGO_URL)
 //   .then(() => console.log("Connected!"))
 //   .catch((e) => console.log(e));
-
+connectDB();
 const userRoute = require("./src/routes/userRoutes");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./docs/swagger");
 const authRoute = require("./src/routes/authRoutes");
 const verifyToken = require("./src/middlewares/authentication");
-const { log } = require("console");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("Home");
-});
-app.get("/test", async (req, res) => {
-  // res.render("Home");
-  try {
-    // await mongoose
-    //   .connect(
-    //     "mongodb+srv://oxfordamreli3:Dhruv123@cluster0.wiihqma.mongodb.net/node_learn"
-    //   )
-    //   .then(() => {
-    //     res.send("Connected");
-    //   })
-    //   .catch((e) => {
-    //     // console.log(e);
-    //     res.send({
-    //       error: e,
-    //     });
-    //   });
-    res.send("hello");
-  } catch (e) {
-    console.log(e);
-    res.send({
-      error: e,
-    });
-  }
 });
 
 app.use(
